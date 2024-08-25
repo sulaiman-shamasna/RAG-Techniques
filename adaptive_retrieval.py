9650e76e2bd97e44598e621bc9ebcbeef2eaa849
Sulaiman Shamasna <sulaiman.shamasna@gmail.com>
2024-08-25 18:40:09 +0200

    chore: :construction_worker: Update adaptive retrieval script - define the query classifier class

--- adaptive_retrieval.py
+++ adaptive_retrieval.py
@@ -24,3 +24,22 @@ load_dotenv()
 # Set the OpenAI API key environment variable
 os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
 
+class categories_options(BaseModel):
+        category: str = Field(description="The category of the query, the options are: Factual, Analytical, Opinion, or Contextual", example="Factual")
+
+
+class QueryClassifier:
+    def __init__(self):
+        self.llm = ChatOpenAI(temperature=0, model_name="gpt-4o", max_tokens=4000)
+        self.prompt = PromptTemplate(
+            input_variables=["query"],
+            template="Classify the following query into one of these categories: Factual, Analytical, Opinion, or Contextual.\nQuery: {query}\nCategory:"
+        )
+        self.chain = self.prompt | self.llm.with_structured_output(categories_options)
+
+
+    def classify(self, query):
+        print("clasiffying query")
+        return self.chain.invoke(query).category
+    
+    
