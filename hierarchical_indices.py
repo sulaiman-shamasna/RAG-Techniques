39ebbd96b2c51dfcfbef172272a6e0fc6b52f12d
Sulaiman Shamasna <sulaiman.shamasna@gmail.com>
2024-08-26 20:56:18 +0200

    feat: :tada: Add new method to enhance RAG system - hierarchial indices

---
+++ hierarchical_indices.py
@@ -0,0 +1,19 @@
+import asyncio
+import os
+import sys
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain.chains.summarize.chain import load_summarize_chain
+from langchain.docstore.document import Document
+from helper_functions import encode_pdf, encode_from_string
+
+
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), '..'))) # Add the parent directory to the path sicnce we work with notebooks
+from helper_functions import *
+from evaluation.evaluate_rag import *
+
+# Load environment variables from a .env file
+load_dotenv()
+
+# Set the OpenAI API key environment variable
+os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
